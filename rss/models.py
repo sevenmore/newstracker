8fa1bf4ea52ef7ce00502b0d74661a49d8021327
sevenmore <sevenmore@gmail.com>
2013-03-29 20:43:11 +0100

    Added app rss, fixed models and added them to admin page

---
+++ rss/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class In(models.Model):
+    id_rss = models.IntegerField(primary_key=True)
+    text = models.CharField(max_length=400)
+    pub_date = models.DateTimeField('date published')
+    url = models.CharField(max_length=400)
+    tags = models.CharField(max_length=400)
+
+
+class Out(models.Model):
+    id_rss = models.IntegerField(primary_key=True)
+    text = models.CharField(max_length=400)
+    pub_date = models.DateTimeField('date published')
+    url = models.CharField(max_length=400)
+    tags = models.CharField(max_length=400)
+
+#class Users(models.Model):
+    #id_user=models.IntegerField(primary_key=True)
+    #username=models.CharField(max_length=20)
+#    subscription = models.ManyToManyField(RssOut, through='Subscription')
+
+class Subscription(models.Model):
+    user = models.ForeignKey(User)
+    subs = models.ForeignKey(Out)

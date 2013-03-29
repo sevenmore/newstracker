a90e3452377c4e383719bc34fcf5906796b356a7
sevenmore <sevenmore@gmail.com>
2013-03-29 20:21:54 +0100

    Just to make sure it merges correctly

--- newstracker/models.py
+++ newstracker/models.py
@@ -8,7 +8,7 @@ class RssIn(models.Model):
     text = models.CharField(max_length=400)
     pub_date = models.DateTimeField('date published')
     url = models.charField(max_length=400)
-	tags = models.charField(max_length=400)
+    tags = models.charField(max_length=400)
 
 
 class RssOut(models.Model):
@@ -21,11 +21,8 @@ class RssOut(models.Model):
 class Users(models.Model):
 	id_user=models.IntegerField(primary_key=True) 
 	username=models.CharField(max_length=20)
-	password=models.CharField(max_length=40)
 	subscription = models.ManyToManyField(RssOut, through='Subscription')
 	
 class Subscription(models.Model):
     user = models.ForeignKey(Users)
     subs = models.ForeignKey(RssOut)
-    
-	
